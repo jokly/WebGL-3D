a381654c5973cbf30c4ed4723a58e5be460f32f1
jokly <slasten32@gmail.com>
2017-05-08 13:50:51 +1000

    Disable the long touch popup

--- src/Game.js
+++ src/Game.js
@@ -258,6 +258,32 @@ window.onresize = function(event) {
 
 document.addEventListener('contextmenu', event => event.preventDefault());
 
+function absorbEvent_(event) {
+    var e = event || window.event;
+    e.preventDefault && e.preventDefault();
+    e.stopPropagation && e.stopPropagation();
+    e.cancelBubble = true;
+    e.returnValue = false;
+    return false;
+}
+
+function preventLongPressMenu(node) {
+    node.ontouchstart = absorbEvent_;
+    node.ontouchmove = absorbEvent_;
+    node.ontouchend = absorbEvent_;
+    node.ontouchcancel = absorbEvent_;
+}
+
+window.disableLongTouch = function() {
+    preventLongPressMenu(document.getElementById('forward'));
+    preventLongPressMenu(document.getElementById('left'));
+    preventLongPressMenu(document.getElementById('backward'));
+    preventLongPressMenu(document.getElementById('right'));
+    preventLongPressMenu(document.getElementById('spawnCube'));
+    preventLongPressMenu(document.getElementById('spawnLight'));
+    preventLongPressMenu(document.getElementById('menu'));
+}
+
 window.onmousemove = function(event) {
     // change camera matrix on mouse move
 };
